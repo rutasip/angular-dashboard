5ba4fad93c65583d4e0e7704612f68b91ad857ad
Ruta Sipkauskaite <ruta.sipkauskaite@gmail.com>
2020-01-14 03:32:02 +0200

    prep for heroku'

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const app = express();
+server.use(middlewares);
+server.use(router);
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-dashboard'));
-
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/angular-dashboard/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
+server.listen(port);
